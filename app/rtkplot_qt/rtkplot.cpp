03bf1ff96e9a624b659cfb6cae70282d4b5404bf
Yonghua Zheng <yh.zheng@yuneec.com>
2021-01-03 00:42:02 +0800

    hotfix for rtkplot_qt stack overflow on Windows

--- app/rtkplot_qt/rtkplot.cpp
+++ app/rtkplot_qt/rtkplot.cpp
@@ -4,8 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Plot w;
-    w.show();
+    Plot* w = new Plot();
+    //Plot w(nullptr);
+    w->show();
 
     return a.exec();
 }
